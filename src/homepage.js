2adada557a89ede971b692655ee81a1dbf9c11ff
Mamitto <user@MBP-User-2.fritz.box>
2020-02-21 22:21:01 +0100

    added files for homepage

---
+++ src/homepage.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './homepage.style.scss';
+const HomePage = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 classname='title'>HATS</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 classname='title'>JACKETS</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 classname='title'>WOMEN</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 classname='title'>MENS</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+
+);
+export default HomePage;
